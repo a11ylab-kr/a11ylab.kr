956e60bc0e2e7fdba1f5febed3601bbbef9340a3
ChanHong Kim <andrea9292@gmail.com>
2026-02-27 11:59:14 +0900

    Update Footer.tsx - Add Copyright text

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          알리의 접근성 연구소. Copyright (c) {year}. All rights reserved.
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
