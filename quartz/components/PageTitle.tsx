f774b5992e69b7adc5279ef7379609a2df0c4c5d
ChanHong Kim <andrea9292@gmail.com>
2026-02-27 11:13:17 +0900

    Update PageTitle.tsx - Adjust Logo and tex's position

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,9 +8,9 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir} style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-        <img src="/static/a11ylab-logo.png" alt="알리의 접근성 연구소 로고" style={{ height: "35px", width: "auto" }} />
-        {title}
+      <a href={baseDir} style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", gap: "12px" }}>
+        <img src="/static/a11ylab-logo.png" alt="알리의 접근성 연구소 로고" style={{ height: "40px", width: "auto" }} />
+        <span style={{ fontSize: "1.1em", lineHeight: "1.3" }}>{title}</span>
       </a>
     </h2>
   )
