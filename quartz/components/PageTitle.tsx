941a04579216f6aaabfa97a11a1d696ffa5cbd79
ChanHong Kim <andrea9292@gmail.com>
2026-03-01 11:00:47 +0900

    Update PageTitle.tsx - Fix multiple root elements issue with <div>

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,13 +7,17 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir} style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2px" }}>
-        <img src="/static/a11ylab-logo.png" alt="알리의 접근성 연구소 로고" style={{ height: "120px", width: "auto" }} />
-        <span style={{ fontSize: "0.7em", lineHeight: "1.2", textAlign: "center" }}>{title}</span>
-      </a>    
-    </h2>
-    <a href="https://hits.sh/a11ylab.kr/"><img alt="방문자 카운터" src="https://hits.sh/a11ylab.kr.svg?view=today-total&style=plastic&color=1818f0"/></a>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }}>
+      <h2 class={classNames(displayClass, "page-title")}>
+        <a href={baseDir} style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2px" }}>
+          <img src="/static/a11ylab-logo.png" alt="알리의 접근성 연구소 로고" style={{ height: "120px", width: "auto" }} />
+          <span style={{ fontSize: "0.7em", lineHeight: "1.2", textAlign: "center" }}>{title}</span>
+        </a>    
+      </h2>
+      <a href="https://hits.sh/a11ylab.kr/">
+        <img alt="방문자 카운터" src="https://hits.sh/a11ylab.kr.svg?view=today-total&style=plastic&color=1818f0"/>
+      </a>
+    </div>
   )
 }
 
